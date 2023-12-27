1775516f4a036495af5c4c879043dc918df1d54b
Evelin <epiljug1@etf.unsa.ba>
2023-12-27 12:54:53 +0100

    adding aws part

--- src/app.js
+++ src/app.js
@@ -14,7 +14,12 @@ const { attachUser } = require("./helpers/jwt");
 
 const app = express();
 const corsOptions = {
-  origin: "http://localhost:3000", // Replace with your React app's URL
+  origin: [
+    "http://localhost:3000",
+    "https://44.199.76.207:3000",
+    "http://44.199.76.207:3000",
+    "http://44.199.76.207",
+  ], // Replace with your React app's URL
   credentials: true, // To allow cookies
 };
 app.use(cors(corsOptions));
@@ -35,7 +40,6 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/posts", postsRouter);
 app.use("/comments", commentRouter);
-
 
 // catch 404 and forward to error handler
 app.use(errorHandler);
